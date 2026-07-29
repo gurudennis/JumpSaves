01d1720b9c52d4f0219a0fae47b0176e8a83f8e2
gurudennis <gurudenis@gmail.com>
2026-07-28 23:53:24 -0400

    Setup doesn't wait for the inital app to close

--- JumpSaves/Program.cs
+++ JumpSaves/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using JSL;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,8 +12,19 @@ namespace JumpSaves
     internal static class Program
     {
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            // Used during Setup only
+            if (args.Length == 1 && args[0] == "/relaunch")
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = Application.ExecutablePath,
+                    UseShellExecute = true
+                });
+                return;
+            }
+
             mutex_ = new Mutex(true, "Global\\JumpSaves-7E864969-4BF6-4548-BC30-F6825F52497D", out bool createdNew);
             if (!createdNew)
             {
